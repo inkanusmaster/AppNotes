60c33d7474f07bcfc4844812e51ad25c7a0b2ed3
inkanusmaster <furymistrz@gmail.com>
2019-10-30 23:16:31 +0100

    Zaczynam. Aplikacja do robienia notatek.

---
+++ app/src/main/java/com/example/section7_appnotes/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.section7_appnotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    boolean firstOpen;
+    ListView notesListView;
+    final ArrayList<String> noteTitle = new ArrayList<>();
+    ArrayAdapter<String> arrayAdapter;
+    protected String title = "";
+
+
+    protected void newNote() {
+        Intent intent = new Intent(this, Note.class);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if (item.getItemId() == R.id.addNote) {
+            newNote();
+            return true;
+        }
+        return false;
+    }
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if(resultCode == RESULT_OK) {
+                title = data.getStringExtra("Title");
+                noteTitle.add(title);
+                notesListView.setAdapter(arrayAdapter);
+            }
+        }
+    }
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        firstOpen = true;
+        notesListView = findViewById(R.id.notesListView);
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, noteTitle);
+        notesListView.setAdapter(arrayAdapter);
+        notesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/section7_appnotes/Note.java
@@ -0,0 +1,33 @@
+package com.example.section7_appnotes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ListView;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Note extends AppCompatActivity {
+
+    EditText titleEditText, contentEditText;
+
+    public void onBackPressed() {
+        try {
+            titleEditText = findViewById(R.id.titleEditText);
+            Intent intent = getIntent();
+            intent.putExtra("Title",titleEditText.getText().toString());
+            setResult(RESULT_OK,intent);
+            finish();
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
+        super.onBackPressed();
+    }
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.note);
+    }
+}
